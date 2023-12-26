2e4188ff7fc89a0a5a5d70c4297c252396cf646d
anandaadisurya <143856184+anandaadisurya@users.noreply.github.com>
2023-12-27 00:56:13 +0700

    Add files via upload

---
+++ resources/views/pratikum_1laravel.blade.php
@@ -0,0 +1,15 @@
+<h1> ini adalah pratikum_1laravel </h1>
+
+<link href="https://stackpath.bootstrapcdn.com/boostrap/4.5.2/css/bootsrap.min.css" rel="stylesheet"
+
+ <a href=/tombol/primary class="btn btn-primary"> Button Primary </a>
+ <a href=/tombol/succes class="btn btn-success"> Button Success </a>
+ <a href=/tombol/danger class="btn btn-danger"> Button Danger </a>
+ <a href=/tombol/warning class="btn btn-warning"> Button warning</a>
+ <a href=/tombol/info class="btn btn-success"> Button Info </a>
+ <a href=/tpmbol/light class="btn btn-danger"> Button Light </a>
+ <a href=/tombol/dark class="btn btn-dark"> Button Dark </a>
+
+ <hr />
+
+ <h2> Jenis tombolnya adalah {{$jenis_tombol}}
---
+++ resources/views/tampil1.blade.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Data Mahasiswa</title>
+    <meta name="csrf.token" conten="{{ csrf_token() }}">
+    <linik rel="stylesheet" href="https://stackpath.bootstrapcdn.com/boostrap/4.5.2/css/bootsrap.min.css">
+</head>
+<body>
+    <div class="container mt-4"->
+    @if(session('status'))
+    </div> class=
+</body>
